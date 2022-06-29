311b1048ca8f5bcbd0e23b463229ab3b83d6559f
bhusalanil121 <bhusalanil121@gmail.com>
2022-06-29 10:06:57 -0400

    TestUrl

--- src/Task13/TestNG.java
+++ src/Task13/TestNG.java
@@ -1,7 +1,32 @@
 package Task13;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class TestNG {
 	WebDriver driver;
+
+	@BeforeTest
+	public void usrlSetUp() {
+//		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+ "/driver/chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+	}
+	
+	@Test
+	public void urlTest() throws InterruptedException {
+		driver.get("https://www.verizon.com/");
+		Thread.sleep(5000);
+	}
+	
+	@AfterTest
+	public void tearUp() {
+		driver.quit();
+	}
+
 }
